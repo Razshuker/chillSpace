b8ce16bffd985002daf85e00cd021a08ef55f8f7
Razshuker <raz.shuker13@gmail.com>
2023-05-29 12:40:19 +0300

    update users router

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const { auth } = require("../middlewares/auth");
-const { validateJoi, UserModel, validateLogin, createToken } = require("../models/userModel");
+const { validateUser, UserModel, validateLogin, createToken, validateUpdateUser } = require("../models/userModel");
 const { route } = require("./posts");
 
 const router = express.Router();
@@ -24,7 +24,7 @@ router.get("/userInfo", auth, async (req, res) => {
 
 
 router.post("/", async (req, res) => {
-  let validBody = validateJoi(req.body);
+  let validBody = validateUser(req.body);
   if (validBody.error) {
     return res.status(400).json(validBody.error.details);
   }
@@ -68,6 +68,67 @@ router.post("/login", async (req, res) => {
 })
 
 
+router.put("/updateUser", auth, async (req, res) => {
+  try {
+    let validBody = validateUpdateUser(req.body);
+    if (validBody.error) {
+      return res.status(400).json(validBody.error.details);
+    }
+    let user = await UserModel.updateOne({ _id: req.tokenData._id }, req.body);
+    res.status(201).json(user);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+router.patch("/changePassword", auth, async (req, res) => {
+  try {
+    let user = await UserModel.findOne({ _id: req.tokenData._id });
+    let passwordValid = await bcrypt.compare(req.body.oldPassword, user.password);
+    if (!passwordValid) {
+      return res.status(401).json({ err: "Password worng, can't delete account" });
+    }
+    let password = await bcrypt.hash(req.body.newPassword, 10);
+    let data = await UserModel.updateOne({ _id: req.tokenData._id }, { password })
+    user.password = "******"
+    res.status(201).json(data);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+router.patch("/addFavorite", auth, async (req, res) => {
+  try {
+    let user = await UserModel.findOne({ _id: req.tokenData._id });
+    let favs_ar = user.favorites;
+    let new_favs_ar = [...favs_ar, req.body.favorite];
+    let data = await UserModel.updateOne({ _id: user._id }, { favorites: new_favs_ar });
+    res.status(200).json(data);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
+
+router.delete("/deleteAccount", auth, async (req, res) => {
+  try {
+    let passwordValid = await bcrypt.compare(req.body.password, user.password);
+    if (!passwordValid) {
+      return res.status(401).json({ err: "Password worng, can't delete account" });
+    }
+    let data = await UserModel.delteOne({ _id: req.tokenData._id });
+    res.status(201).json(data);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
 
 
 module.exports = router;
