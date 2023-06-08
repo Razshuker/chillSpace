2ba40cf814fe74109256778046e19d8d4b19e803
Razshuker <raz.shuker13@gmail.com>
2023-06-08 14:01:52 +0300

    add usersList router

--- routes/users.js
+++ routes/users.js
@@ -10,11 +10,21 @@ router.get("/", async (req, res) => {
   res.json({ msg: "Users endpoint" });
 })
 
+router.get("/usersList", authAdmin, async (req, res) => {
+  try {
+    let data = await UserModel.find({}, { password: 0 })
+    res.status(200).json(data)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(502).json({ err })
+  }
+})
 
 router.get("/userInfo", auth, async (req, res) => {
   try {
     let user = await UserModel.findOne({ _id: req.tokenData._id }, { password: 0 })
-    res.json(user)
+    res.status(200).json(user)
   }
   catch (err) {
     console.log(err);
