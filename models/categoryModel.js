49c07a93f0a6205a18f40bb9f2ce14f7bf5e84e1
Razshuker <raz.shuker13@gmail.com>
2023-06-08 23:21:03 +0300

    .

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -13,8 +13,8 @@ exports.CategoryModel = mongoose.model("categories", schema)
 
 exports.validateCategory = (_reqBody) => {
     let joiSchema = Joi.object({
-        name: Joi.string().min(2).max(100).required(),
-        description: Joi.string().min(2).max(400).required(),
+        name: Joi.String().min(2).max(100).required(),
+        description: Joi.String().min(2).max(400).required(),
         category_code: Joi.String().min(2).max(50).required()
     })
     return joiSchema.validate(_reqBody)
