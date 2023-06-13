89039fd604a3d9394602330ea074425b9e9038d9
noyNosrati <noynosrati@gmail.com>
2023-06-13 09:17:22 +0300

    fixing place model

--- models/placeModel.js
+++ models/placeModel.js
@@ -5,17 +5,17 @@ let schema = new mongoose.Schema({
     img_url: String,
     name: String,
     type: String,
-    categories_code: Array,
     city: String,
     phone: String,
     location: Object,
     description: String,
     open_hours: Object,
     area: String,
+    tags_name: Array,
+    categories_code: Array,
     date_created: {
         type: Date, default: Date.now
-    },
-    tags_name: Array
+    }
 })
 exports.PlaceModel = mongoose.model("places", schema)
 
@@ -23,15 +23,15 @@ exports.validatePlace = (_reqBody) => {
     let joiSchema = Joi.object({
         img_url: Joi.string().min(2).max(400).allow(null, ""),
         name: Joi.string().min(2).max(100).required(),
-        phone: Joi.string().min(4).max(25).allow(null, ""),
         type: Joi.string().min(2).max(100).required(),
-        categories_id: Joi.array().min(0).max(99).allow(null, ""),
         city: Joi.string().min(2).max(100).required(),
-        location: Joi.object().min(2).max(2).required(),
+        phone: Joi.string().min(4).max(25).allow(null, ""),
+        location: Joi.object().min(2).max(25).required(),
         description: Joi.string().min(2).max(1500).required(),
         open_hours: Joi.object().min(1).max(7).required(),
         area: Joi.string().min(2).max(100).required(),
-        tags_id: Joi.array().min(0).max(100).allow(null, ""),
+        tags_name: Joi.array().min(0).max(100).allow(null, ""),
+        categories_code: Joi.array().min(0).max(99).allow(null, ""),
     })
     return joiSchema.validate(_reqBody)
 }
