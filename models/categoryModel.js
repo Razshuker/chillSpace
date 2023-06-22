885b9dd5120de089fad71a28452bf3b0ad9e49b3
Razshuker <raz.shuker13@gmail.com>
2023-06-22 11:33:25 +0300

    add img_url - categories model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -5,6 +5,7 @@ let schema = new mongoose.Schema({
     name: String,
     description: String,
     category_code: String,
+    img_url: String,
     date_created: {
         type: Date, default: Date.now
     },
@@ -14,6 +15,7 @@ exports.CategoryModel = mongoose.model("categories", schema)
 exports.validateCategory = (_reqBody) => {
     let joiSchema = Joi.object({
         name: Joi.string().min(2).max(100).required(),
+        img_url: Joi.string().min(2).max(250).allow(null, ""),
         description: Joi.string().min(2).max(400).required(),
         category_code: Joi.string().min(2).max(50).required()
     })
